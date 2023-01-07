bd5ac71d71537987500847ebe299ec6a10858bc9
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-08 02:18:47 +0300

    Focus on input when editing | useRef hook and UseEffect

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Todo } from "../model";
 import "./styles.css";
 
@@ -34,10 +34,20 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
     setEdit(false);
   };
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
   return (
     <form className="todos__single" onSubmit={(e) => handleSubmit(e, todo.id)}>
       {edit ? (
-        <input value={editTodo} onChange={(e) => setEditTodo(e.target.value)} />
+        <input
+          ref={inputRef}
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+        />
       ) : todo.isDone ? (
         // s is the strike tag
         <s className="todos__single-text">{todo.todo}</s>
