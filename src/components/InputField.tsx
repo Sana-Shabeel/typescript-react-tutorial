771018b3b8407c689979dc665a30c9df2c4d5d2a
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-07 10:18:00 +0300

    useState in typescript

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({ todo, setTodo }: Props) => {
   return (
     <form className="input">
-      <input type="text" placeholder="Enter a task" className="input__box" />
+      <input
+        type="text"
+        placeholder="Enter a task"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+      />
       <button className="input__submit" type="submit">
         GO
       </button>
