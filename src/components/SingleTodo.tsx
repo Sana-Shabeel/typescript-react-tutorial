f010d189de29444aa86cc7212e6870543fb494ef
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-08 02:03:15 +0300

    Add functionality for Edit and delete task

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Todo } from "../model";
 import "./styles.css";
 
@@ -9,14 +9,47 @@ interface Props {
 }
 
 const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <form className="todos__single">
-      <span className="todos__single-text">{todo.todo}</span>
+      {todo.isDone ? (
+        // s is the strike tag
+        <s className="todos__single-text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single-text">{todo.todo}</span>
+      )}
 
       <div>
-        <span className="icon">✍️</span>
-        <span className="icon">❌</span>
-        <span className="icon">✔️</span>
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
+          ✍️
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          ❌
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          ✔️
+        </span>
       </div>
     </form>
   );
